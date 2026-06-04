61e43881989c6b99f0468a2ad3660427ecf53954
mitssky <echeverrianicole71@gmail.com>
2026-06-04 15:01:04 -0600

    prac7 react, incompleta

---
+++ p7.react copy/mi-primer-react/src/MiComponente.jsx
@@ -0,0 +1,10 @@
+
+
+export default function MiComponente() {
+    return(
+        <section className="card">
+            <h2 className="card-tittle">{titulo}</h2>
+            <h3 className="card-subtitle">{ligar} </h3>
+        </section>
+    )
+}
